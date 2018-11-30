dad3928399ee8806a96a700f5640e6fcca2c4546
nach7vos <joshwow91@gmail.com>
2018-11-30 21:19:13 +0100

    code styling

--- Python/Sensors.py
+++ Python/Sensors.py
@@ -1,14 +1,19 @@
-import sys 
-import RPi.GPIO as GPIO 
-from time import sleep 
-import Adafruit_DHT 
-from urllib.request import urlopen
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+import sys
 import threading
+from time import sleep
+from urllib.request import urlopen
 
 
-def getSensorData():
-   threading.Timer(900.0, getSensorData).start() 
-   RH, T = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22,4) 
-   print(str(T))
-   print(str(RH))   
-getSensorData()   
+def get_sensor_data():
+    threading.Timer(900.0, getSensorData).start()
+    RH, T = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+    print(str(T))
+    print(str(RH))
+
+
+print('Blah')
+
+
+
