d9eff5cf67025e985b934fdd4db2a49d327f5c55
nach7vos <joshwow91@gmail.com>
2018-12-02 19:12:53 +0100

    having fun, understanding it, thank you god

--- Python/Nachos.py
+++ Python/Nachos.py
@@ -1,24 +1,55 @@
 import numpy as np
 import cv2
+from collections import deque
 
+#   start video from file
 cap = cv2.VideoCapture("walking.avi")
-fullBodyCascade = cv2.CascadeClassifier('cascades/haarcascade_fullbody.xml')
+#   add haarcascades
+full_body_cascade = cv2.CascadeClassifier('cascades/haarcascade_fullbody.xml')
+upper_body_cascade = cv2.CascadeClassifier('cascades/haarcascade_upperbody.xml')
+upper_body_pts = deque()
+
+
 while(cap.isOpened()):
     #   Capture frame-by-frame
     ret, frame = cap.read()
+    #   frame width & height
     FRAME_WIDTH = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     FRAME_HEIGHT = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     #   Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #   Cascades
-    bodies = fullBodyCascade.detectMultiScale(frame, scaleFactor=1.15, minNeighbors=3, minSize=(20, 20))
-    for (x, y, width, height) in bodies:
-        cv2.rectangle(frame,(x, y),(x + width,  y + height), (0 , 255, 0), 1)
-        #   ROI = Region of Interest, aldus waar de object zich nu bevind
+    
+    #   cascades detect multiscale
+    full_bodies = full_body_cascade.detectMultiScale(frame, scaleFactor=1.25, minNeighbors=3)
+    upper_bodies = upper_body_cascade.detectMultiScale(frame, scaleFactor=1.15, minNeighbors=3) 
+    #   X Y grid
+    cv2.line(frame, (0, 240), (640, 240), (0, 255, 0), 1) 
+    cv2.line(frame, (320, 0), (320, 480), (0, 255, 0), 1)
+
+    #   Upperbodies cascade detection
+    for(x, y, width, height) in upper_bodies:
+        #   put dot in the center of detected upper body
+        center = (int((x+width) - (height/2)), int((y+height) - (height/2)))
+        cv2.circle(frame, (center), 3, (0, 255, 0), 2) 
+        upper_body_pts.append(center)
+        
         roi_gray = gray[y:y+height, x:x+width]
-        roi_color = frame[y:y+height, x:x+width]   
+        roi_color = frame[y:y+height, x:x+width]
+    
+    #   Full bodies cascade detection
+    for (x, y, width, height) in full_bodies:
+        cv2.rectangle(frame,(x, y),(x+width,  y+height), (0, 0, 255), 1)  
+        
+        roi_gray = gray[y:y+height, x:x+width]
+        roi_color = frame[y:y+height, x:x+width]
+    
+    if len(upper_body_pts) > 1:
+        for i in range(1, len(upper_body_pts)):
+            thickness = int(i * 1.10)
+            cv2.line(frame, upper_body_pts[i-1], upper_body_pts[i], (255, 0, 0), thickness)
+
     #   Display the resulting frame
-    cv2.imshow('bitch', frame)
+    cv2.imshow('nigh7fox', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
@@ -26,3 +57,4 @@ while(cap.isOpened()):
 #   When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
