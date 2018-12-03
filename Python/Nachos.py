3f7a0025bb2dadff74102a328b4f49fecbf21c9e
nach7vos <joshwow91@gmail.com>
2018-12-03 20:30:38 +0100

    going for some weed, happy with the current workings, next is making each detected object unique for count

--- Python/Nachos.py
+++ Python/Nachos.py
@@ -6,7 +6,7 @@ import imutils
 from imutils.object_detection import non_max_suppression
 
 
-MAX_BUFFER = 8 
+MAX_BUFFER = 16 
 #   add haarcascades
 full_body_cascade = cv2.CascadeClassifier('cascades/haarcascade_fullbody.xml')
 upper_body_cascade = cv2.CascadeClassifier('cascades/haarcascade_upperbody.xml')
@@ -67,45 +67,52 @@ def haar_detect():
 def hog_detect():
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-    hog_bodies = deque(maxlen=MAX_BUFFER) 
+    hog_bodies = deque(maxlen=MAX_BUFFER)
     #   start video from file
-    cap = cv2.VideoCapture("walking_sample4.mp4")
+    cap = cv2.VideoCapture("walking_sample7.mp4")
     start_time = time.time()
+    right_counter = 0
+    left_counter = 0
     while(cap.isOpened()):
         ret, frame = cap.read()
-        frame = imutils.resize(frame, width=min(400, frame.shape[1]))
+        frame = imutils.resize(frame, width=min(600, frame.shape[1])) 
         orig = frame.copy()
+        (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.30) 
+        cv2.line(frame, (100, 0), (100, 600), (0, 0, 255), 1)
+        cv2.line(frame, (350, 0), (350, 600), (0, 0, 255), 1)
         
-        (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.20)
-        for (x, y, w, h) in rects:
-            cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-        #   apply non-maxima suppression to the bounding boxes using a
-	#   fairly large overlap threshold to try to maintain overlapping
-	#   boxes that are still people
-        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-        pick = non_max_suppression(rects, probs=None, overlapThresh=0.70)
-	# draw the final bounding boxes
-        for (xA, yA, xB, yB) in pick:
-            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+        for(x, y, w, h) in rects:
+            #   track centers of detected objects
             center = (int((x+w) - (w/2)), int((y+h) - (h/2)))
             hog_bodies.append(center)
             cv2.circle(frame, (center), 3, (0, 255, 0), 2) 
+            #   contrail
+            if len(hog_bodies) > 1:
+                for i in range(1, len(hog_bodies)):
+                    #   MAX_BUFFER = length of deque for contrail
+                    #   a bigger length will have a longer tail
+                    thickness = int(np.sqrt(MAX_BUFFER / float(i+1)) * 2.5)
+                    cv2.line(frame, hog_bodies[i-1], hog_bodies[i], (0, 0, 255), thickness)
+
+        #   apply non-maxima suppression to the bounding boxes using a
+        #   fairly large overlap threshold to try to maintain overlapping
+	#   boxes that are still people 
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.6)	
         
-        if len(hog_bodies) > 1:
-            for i in range(1, len(hog_bodies)):
-                thickness = int(np.sqrt(MAX_BUFFER / float(i+1)) * 2.5)
-                cv2.line(frame, hog_bodies[i-1], hog_bodies[i], (0, 0, 255), thickness)
-
-
-        end_time = time.time()
-        cv2.putText(frame, "Time Elapsed: " + str(end_time-start_time), (20, 20), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
+        
+        # draw the final bounding boxes
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 1)
+        
+        end_time = time.time() 
+        cv2.putText(frame, "Right counter: " + str(right_counter), (20, 40), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA) 
+        cv2.putText(frame, "Left counter: " + str(left_counter), (20, 60), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA) 
+        cv2.putText(frame, "Time Elapsed: " + str(end_time-start_time), (20, 20), font, 0.5, (255, 0, 0), 1, cv2.LINE_AA) 
         cv2.imshow("HOG with Non Max Suppression", frame)
-
         #   Display the resulting frame
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
 
 
 hog_detect()
