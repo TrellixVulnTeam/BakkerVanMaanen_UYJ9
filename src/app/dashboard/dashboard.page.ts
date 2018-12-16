2d53f011f678507d10121960c99b75b61651d6b5
Joshua Tromp <joshwow91@gmail@github.com>
2018-12-16 21:13:30 +0100

    switching dashboard mode now possible

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
-  constructor() { }
+ analyticsMode = false;
+ phoneMode = true;
 
+ constructor() { }
+
+  toggleView(mode: number) {
+      if (mode === 1) {
+          this.analyticsMode = true;
+          this.phoneMode = false;
+      }
+      if (mode === 0) {
+          this.phoneMode = true;
+          this.analyticsMode = false;
+      }
+  }
   ngOnInit() {
   }
 
--- src/app/dashboard/temperature-analytics/temperature-analytics.component.ts
+++ src/app/dashboard/temperature-analytics/temperature-analytics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-temperature-analytics',
@@ -6,8 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./temperature-analytics.component.scss']
 })
 export class TemperatureAnalyticsComponent implements OnInit {
+  temperatureLineChart: Chart;
+  dataLoaded = false;
 
-  constructor() { }
+  constructor() { 
+    this.createTemperatureLineChart();
+  }
+
+  createTemperatureLineChart() {
+      this.temperatureLineChart = new Chart('temperatureLineChart', {
+          type: 'line',
+          data: ['1', '2', '3'],
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }]
+              }
+          }
+      });
+      this.dataLoaded = true;
+  }
 
   ngOnInit() {
   }
