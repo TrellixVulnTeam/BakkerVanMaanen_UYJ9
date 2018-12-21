a9a22c53ae36f1958f81233a7460ddb8d32b6cd1
Joshua Tromp <joshwow91@gmail@github.com>
2018-12-21 21:28:38 +0100

    yeee analytics bwoi

--- src/app/dashboard/temperature-analytics/temperature-analytics.component.ts
+++ src/app/dashboard/temperature-analytics/temperature-analytics.component.ts
@@ -10,28 +10,49 @@ export class TemperatureAnalyticsComponent implements OnInit {
   temperatureLineChart: Chart;
   dataLoaded = false;
 
-  constructor() { 
-    this.createTemperatureLineChart();
+  constructor() {
   }
 
   createTemperatureLineChart() {
-      this.temperatureLineChart = new Chart('temperatureLineChart', {
-          type: 'line',
-          data: ['1', '2', '3'],
-          options: {
-              scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero: true
-                      }
-                  }]
-              }
-          }
-      });
-      this.dataLoaded = true;
+    const ctx = document.getElementById('temperatureLineChart');
+    this.temperatureLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Temperatuur', 'Vochtigheid'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: ['rgba(255, 99, 132, 0.2)',
+                                  'rgba(54, 162, 235, 0.2)',
+                                  'rgba(255, 206, 86, 0.2)',
+                                  'rgba(75, 192, 192, 0.2)',
+                                  'rgba(153, 102, 255, 0.2)',
+                                  'rgba(255, 159, 64, 0.2)'],
+                borderColor: ['rgba(255,99,132,1)',
+                              'rgba(54, 162, 235, 1)',
+                              'rgba(255, 206, 86, 1)',
+                              'rgba(75, 192, 192, 1)',
+                              'rgba(153, 102, 255, 1)',
+                              'rgba(255, 159, 64, 1)'],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                         beginAtZero: true
+                    }
+                }]
+            }
+        }
+
+    });
+    this.dataLoaded = true;
   }
 
   ngOnInit() {
+    this.createTemperatureLineChart();
   }
 
 }
--- src/app/dashboard/vitrine-analytics/vitrine-analytics.component.ts
+++ src/app/dashboard/vitrine-analytics/vitrine-analytics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-vitrine-analytics',
   templateUrl: './vitrine-analytics.component.html',
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VitrineAnalyticsComponent implements OnInit {
 
-  constructor() { }
+  vitrineChart: Chart;
+  dataLoaded = false;
+
+  constructor() {}
+
+  createVitrineChart() {
+    const ctx = document.getElementById('vitrineChart');
+    this.vitrineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Temperatuur', 'Vochtigheid'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: ['rgba(255, 99, 132, 0.2)',
+                                  'rgba(54, 162, 235, 0.2)',
+                                  'rgba(255, 206, 86, 0.2)',
+                                  'rgba(75, 192, 192, 0.2)',
+                                  'rgba(153, 102, 255, 0.2)',
+                                  'rgba(255, 159, 64, 0.2)'],
+                borderColor: ['rgba(255,99,132,1)',
+                              'rgba(54, 162, 235, 1)',
+                              'rgba(255, 206, 86, 1)',
+                              'rgba(75, 192, 192, 1)',
+                              'rgba(153, 102, 255, 1)',
+                              'rgba(255, 159, 64, 1)'],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                         beginAtZero: true
+                    }
+                }]
+            }
+        }
+
+    });
+    this.dataLoaded = true;
+  }
 
   ngOnInit() {
+      this.createVitrineChart();
   }
 
 }
