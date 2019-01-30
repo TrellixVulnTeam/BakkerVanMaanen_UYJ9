a69f6be4ce54b04e520eb518a0705b94b4e89955
Reza Naser <rezanm2@gmail.com>
2019-01-30 16:40:48 +0100

    Update temp.py

--- Python/temp.py
+++ Python/temp.py
@@ -13,8 +13,20 @@ def getSensorData():
                 {'temp_sensor_no':2,'temperature': T2,'humidity':RH2},
                 {'temp_sensor_no':3,'temperature': T3,'humidity':RH3}
     ]
-    Bakkerbase.save_temperature(temperature_data)
+    try:
+        Bakkerbase.save_temperature(temperature_data)
+        print('..Data verzonden naar api')
+        for temp in temperature_data:
+            try:
+                if int(temp['temperature']) < 16 or int(temp['temperature']) > 21:
+                    #notificatione = 'Sensor: '. temp['temp_sensor_no'] . ' is buiten de wettelijke norm.'
+                    Bakkerbase.save_notofication('Tempratuur', 'Sensor waarde is buiten de wettelijke norm.')
+            except ConnectionError:
+                print("Connection error, maar hij gaat door")  
+    except ConnectionError:
+        print("Connection error, maar hij gaat door")
+    
 
 while True: 
-    time.sleep(300)
+    time.sleep(600)
     getSensorData()
